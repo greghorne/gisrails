b448df833ab2873b41f884feaf665e3cb5a16d66
Greg Horne <greghorne@hotmail.com>
2015-03-16 11:38:27 -0500

    REST sleep adjusted to .05 sec

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -98,7 +98,7 @@ class MapController < ApplicationController
       threads[counter] = Thread.new {
         Thread.current["response"] = RestClient.get getString 
       }
-      sleep (0.75)
+      sleep (0.05)
       counter = counter + 1
       n = n + 1
     end
